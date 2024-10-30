bc3c10c173dca9c423addc629823f528b7bf1168
alexdgbs <alexdbc24@gmail.com>
2024-10-29 18:20:45 -0600

    HSA static

---
+++ dist/_nuxt/static/1730247142/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fteam","\u002F","\u002Fcancel","\u002Finicio","\u002Fpong","\u002Fgba","\u002Fpsp","\u002Flogin","\u002Fps2","\u002Fprecios","\u002Fn64","\u002Fsnes","\u002Fsuccess","\u002Femuladores","\u002Fcheckout","\u002Fgamecube","\u002Fconsolas","\u002Fregister"]})
---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+export default {
+  head: {
+    title: 'hsa-games',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+    ],
+    script: []
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    '@nuxt/postcss8', 
+    '@nuxtjs/tailwindcss', 
+  ],
+
+  modules: [],
+
+  build: {},
+
+  target: 'static',
+  generate: {
+    dir: 'dist',
+  },
+  
+  env: {
+    API_URL: 'https://api.hsa-games.com' 
+  },
+
+  server: {
+    port: process.env.PORT || 3000, 
+    host: '0.0.0.0' 
+  },
+}
